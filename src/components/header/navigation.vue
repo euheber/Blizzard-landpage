7f5ddc38feeab31643b90c7086753780862eb41c
euheber <euheber1@gmail.com>
2022-11-28 20:12:04 -0300

    add: nav component

---
+++ src/components/header/navigation.vue
@@ -0,0 +1,16 @@
+<template>
+    <i class="fa-solid fa-bars mobile-btn"></i>
+</template>
+
+<script>
+export default {
+    name: 'navigation',
+}
+</script>
+
+<style lang="scss" scoped>
+.mobile-btn {
+    font-size: 2rem;
+    color: white;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,18 +1,56 @@
 <template>
-  <h1>Olá</h1>
+  <header>
+    <nav>
+      <a href="" target="_blank">
+        <img src="../assets/logo-blizzard.png" alt="Blizzard">
+      </a>
+      <navigation />
+    </nav>
+    <div class="divider"></div>
+  </header>
 </template>
 
 <script>
+
+import navigation from '../components/header/navigation.vue'
 export default {
-    name: 'Home'
+  name: 'Home',
+  components: {
+    navigation
+  }
 }
 </script>
+<style lang="scss" scoped>
+header {
+  min-height: 625px;
+  width: 100%;
+  background-image: url('../assets/bg-banners/bg-banner-diablo.jpg');
+  background-position: center right -550px;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 
-<style lang="scss">
-  $cor: lightblue;
+nav {
+  padding: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
-  h1{
-    color: $cor;
+.divider {
+  border: 1.5px solid rgba(255, 255, 255, 0.1);
+  width: 100%;
+  height: 5px;
+  position: relative;
+
+  &::after {
+    position: absolute;
+    content: '';
+    width: 43px;
+    height: 3px;
+    background-color: #00AEFF;
+    left: 1.5rem;
+    bottom: 0px;
   }
-
+}
 </style>
