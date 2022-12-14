8f15b2fc8977d8f978dac7d8e44fe79c91717f5a
euheber <euheber1@gmail.com>
2022-12-14 18:13:56 -0300

    fixed: base info

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Blizzard-landpage',
+  base:'/Blizzard-landpage/',
   plugins: [vue()],
   resolve: {
     alias: {
