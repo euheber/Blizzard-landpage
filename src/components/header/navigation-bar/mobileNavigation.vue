4ac3ab0441fc24f51cf5ff098e7ac8f24a162ff8
euheber <euheber1@gmail.com>
2022-12-15 21:10:05 -0300

    add: mobile transition

--- src/components/header/navigation-bar/mobileNavigation.vue
+++ src/components/header/navigation-bar/mobileNavigation.vue
@@ -3,21 +3,20 @@
         <button id="openMobileMenu" @click="this.handleMobileMenu = !this.handleMobileMenu">
             <i class="fa-solid fa-bars mobile-btn"></i>
         </button>
-
-        <div id="mobileMenu" v-if="handleMobileMenu">
-            <button @click="handleGameList">Jogos<i class="fa-solid fa-chevron-down btnGame"></i></button>
+        <Transition name="grow">
+            <div id="mobileMenu" v-if="handleMobileMenu">
+            <div id="listBox">
+                <button @click="handleGameList" >Jogos<i class="fa-solid fa-chevron-down btnGame"></i></button>
             <ul v-if="toggleGamesList">
                 <li>Jogos</li>
             </ul>
 
             <button @click="handleEsportList">Esportes<i class="fa-solid fa-chevron-down btnEsport"></i></button>
-           <transition>
+  
             <ul v-if="toggleEsportList">
-                <li>
-                    Esportes
-                </li>
+                <li>Esportes</li>
             </ul>
-           </transition>
+
 
             <ul>
                 <li>
@@ -31,11 +30,17 @@
                     <p>Suporte</p>
                 </li>
             </ul>
+            </div>
 
             <div id="mobileMenuFooter">
-                <p style="color: white">teste</p>
+                <img src="/icons-banner-hero/blizzardIcon.svg" alt="">
+                <img src="/icons-banner-hero/allGames.svg" alt="">
+                <img src="/icons-banner-hero/comunityIcon.svg" alt="">
+                <img src="/icons-banner-hero/downloadIcon.svg" alt="">
             </div>
         </div>
+        </Transition>
+       
     </div>
 </template>
 
@@ -77,12 +82,12 @@ export default {
 <style lang="scss" scoped>
 
 *{
-    text-align: left;
+    text-align: center;
 }
-#mobileMenu {
-    z-index: 99;
-    background: linear-gradient(180deg, #020203 0%, rgba(14, 17, 23, 0.92) 96.35%, rgba(14, 17, 23, 0.9) 100%);
-    backdrop-filter: blur(6px);
+
+#listBox{
+    margin-top: 2rem;
+    margin: 0 auto;
 }
 
 #openMobileMenu {
@@ -99,18 +104,20 @@ export default {
     top: 0;
     width: 100%;
     z-index: 98;
-    min-height: 60vh;
+    // min-height: 60vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
-
+    background: linear-gradient(180deg, #020203 0%, rgba(14, 17, 23, 0.92) 96.35%, rgba(14, 17, 23, 0.9) 100%);
+    backdrop-filter: blur(6px);
     button {
         background-color: transparent;
         color: white;
         display: flex;
         gap: 14px;
         align-items: center;
-        width: 25%;
+        width: 100%;
+        justify-content: center;
     }
 
     ul li {
@@ -123,13 +130,17 @@ export default {
     align-self: end;
     position: absolute;
     bottom: 0;
-    border: 1px solid #00AEFF;;
     width: 100%;
+    background-color: black;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: .5rem;
 }
 
 .btnGame {
     transition: .3s ease;
-
     &.active {
         transform: rotate(3.142rad);
         color: #00AEFF;;
@@ -144,6 +155,31 @@ export default {
         transform: rotate(3.142rad);
         color: #00AEFF;;
     }
+}
 
+.grow-enter-from{
+    height: 0;
+}
+
+.grow-enter-active{
+    transition: all 1s ease;
+    min-height: 60vh;
+}
+
+.grow-enter-to{
+    min-height: 60vh;
+}
+
+.grow-leave-from{ 
+    min-height: 60vh;
+}
+
+.grow-leave-active{ 
+    transition: all 1s ease;
+    height: 0;
+}
+
+.grow-leave-to{
+    height: 0;
 }
 </style>
