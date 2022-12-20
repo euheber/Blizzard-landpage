ff6f3b258b1b3df3c3af9831402d83b30fbfdd45
euheber <euheber1@gmail.com>
2022-12-20 14:03:02 -0300

    add: scss folder configuration

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/style/main.css'
+import './assets/default.css'
 
 const app = createApp(App)
 
