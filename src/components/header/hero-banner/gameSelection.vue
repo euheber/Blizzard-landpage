94ca30df7a904808dd62c26c24f34c1acd0df678
euheber <euheber1@gmail.com>
2022-12-26 17:55:50 -0300

    add: logo and  gif

--- src/components/header/hero-banner/gameSelection.vue
+++ src/components/header/hero-banner/gameSelection.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="game-selection">
-    <selectGameIconVue @getData="gotData" class="testeD" />
+  <div class="position-relative-container">
+    <div class="position-absolute-container">
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <button>{{ cta }}</button>
+    </div>
+    <selectGameIconVue @getData="gotData" />
+  </div>
 
-    <div class="position-relative-container">
-      <div class="position-absolute-container">
-        <h1>{{ title }}</h1>
-        <p>{{ description }}</p>
-        <button>{{ cta }}</button>
-      </div>
+
+    <div id="logo-gif">
+      <img :src="logo" alt="teste" class="logo">
+      <img :src="gif" alt="teste" class="gif">
     </div>
   </div>
 </template>
@@ -21,18 +26,22 @@ export default {
     title: 'Retorne à escuridão com o game Diablo IV',
     description: 'O retorno de Lilith traz uma era de escuridão e sofrimento',
     cta: 'Jogue agora',
+    logo: '/icons-banner-hero/diabloLogo.svg',
+    gif: '/icons-banner-hero/diabloIV.gif'
   }),
   methods: {
-    gotData({ title, bg, description, cta, highlight, divider }) {
+    gotData({ title, bg, description, cta, highlight, divider, logo, gif}) {
       this.title = title
-      this.background = bg
       this.description = description
       this.cta = cta
-
+      this.logo = logo,
+      this.gif = gif
       this.$emit('getBg', { bg, highlight, divider })
     }
   }
 }
 </script>
+
+
 
 <style src="../../../assets/scss/header/gameSelection.scss" lang="scss" scoped />
